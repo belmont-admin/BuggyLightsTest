7f69721fc06cffea0c729302d62fdcc740871ac0
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2020-02-02 14:22:26 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+input.onButtonPressed(Button.B, function () {
+    PixelArray.clear()
+    PixelArray.show()
+})
+input.onButtonPressed(Button.A, function () {
+    PixelArray.showRainbow(1, 360)
+})
+let PixelArray: neopixel.Strip = null
+PixelArray = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+basic.forever(function () {
+    PixelArray.rotate(1)
+    PixelArray.show()
+    basic.pause(200)
+})
